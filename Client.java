831d968f9fcccff9dc12a6a1cedd9046944f9afd
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-12 19:33:33 -0800

    Delete Client.java

--- Client.java
+++
@@ -1,8 +0,0 @@
-public class Client {
-    private String serverAddress;
-    private Socket socket;
-
-    public void connect(String addr) { }
-    public void send(Message msg) { }
-    public void disconnect() { }
-}
