3137d6f70f0dbc2cecc33ad47ced8593c5b6fdb1
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-12 19:15:06 -0800

    Create ClientController.java

---
+++ ClientController.java
@@ -0,0 +1,22 @@
+public class ClientController{
+  private Clientlistener clientListener;
+  private Client client;
+  private parkingAppGui appGUI;
+  private User currentUser;
+
+  public void start(){
+  }
+  public boolean sendLoginRequest(String u, String p){
+    return false;
+  }
+  public void sendParkingRequest(PaarkingMessage req){
+  }
+  public voidhandleServerResponse(parkingResponse res){
+  }
+  public User getCurrentUser(){
+    return GetCurrentUser;
+  }
+  public void setCurrentUser(User u){
+    this.currentUser = u;
+  }
+}
