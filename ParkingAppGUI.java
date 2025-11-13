945c16a40f6508bb78ca7be1394c08913faa1904
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-12 19:17:23 -0800

    Create ParkingAppGUI.java

---
+++ ParkingAppGUI.java
@@ -0,0 +1,12 @@
+public class ParkingAppGUI{
+    private ClientController controller;
+
+    public void showOccupancy(int pct){
+    }
+    public void showTicker(TicketDTO t){
+    }
+    public void onCheckIn(String plate, String type){
+    }
+    public voif onCheckOut(String TicketID){
+    }
+}
