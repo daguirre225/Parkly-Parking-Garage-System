0f65e86b57f947de8e5f2e798e7acd4338a26a6d
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-12 19:36:45 -0800

    Create EmployeeGUI.java

---
+++ EmployeeGUI.java
@@ -0,0 +1,10 @@
+public class EmployeeGUI{
+    private String userName;
+    private String password;
+
+    public boolean login(String user, String pass){
+      return false;
+    }
+    public void logout(){
+    }
+}
