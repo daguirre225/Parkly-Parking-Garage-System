42b6369dd3ec87c1842ead49688071ff359eb356
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-12 19:05:52 -0800

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,8 @@
+public class Client {
+    private String serverAddress;
+    private Socket socket;
+
+    public void connect(String addr) { }
+    public void send(Message msg) { }
+    public void disconnect() { }
+}
