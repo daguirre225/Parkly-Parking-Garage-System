1c328f78a5fda52d634d2d993db849a9e667f91c
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-13 19:51:41 -0800

    Create Report.java

---
+++ Report.java
@@ -0,0 +1,9 @@
+public class Report {
+    private Date date;
+    private int carsParked;
+    private float revenue;
+    private String peakHours;
+
+    public Report generateDaily() { return null; }
+    public String exportCSV() { return ""; }
+}
