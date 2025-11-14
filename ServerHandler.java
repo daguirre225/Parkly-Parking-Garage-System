2c1d30c78ead9d4b9b2b31f004cbed7538f29f61
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-13 19:50:03 -0800

    Create ServerHandler.java

---
+++ ServerHandler.java
@@ -0,0 +1,7 @@
+public class ServerHandler implements Runnable {
+    private BufferedReader inputStream;
+    private PrintWriter outputStream;
+    private Socket socket;
+
+    public void run() { }
+}
