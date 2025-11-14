65a397a0e7bb375c1d145967d847282b6653b481
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-13 19:54:12 -0800

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,7 @@
+public class Customer {
+    private String id;
+    private Payment payment;
+
+    public Ticket distributeTicket() { return null; }
+    public boolean pay(Ticket t) { return false; }
+}
