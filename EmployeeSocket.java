c4a41e5aef1b3c0caf246cd8346ae48777524400
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-13 19:49:03 -0800

    Create EmployeeSocket.java

---
+++ EmployeeSocket.java
@@ -0,0 +1,7 @@
+public class EmployeeSocket {
+    private String userName;
+    private String password;
+
+    public boolean login(String user, String pass) { return false; }
+    public Socket createSocket() { return null; }
+}
