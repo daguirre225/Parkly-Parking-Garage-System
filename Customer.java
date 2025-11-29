810291b8b23384017b66e113d9c819b130c9c700
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-28 21:22:49 -0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,6 +2,24 @@ public class Customer {
     private String id;
     private Payment payment;
 
-    public Ticket distributeTicket() { return null; }
-    public boolean pay(Ticket t) { return false; }
+    public Customer(String id) {
+        this.id = id;
+        this.payment = null;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public Ticket distributeTicket() {
+        return null;
+    }
+
+    public boolean pay(Ticket t) {
+        return false;
+    }
 }
