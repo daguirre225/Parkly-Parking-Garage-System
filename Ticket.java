832106e29361fc31b0c7f273a2f8c0d4326e7d3e
Lucas-chabot <75271837+Lucas-chabot@users.noreply.github.com>
2025-11-28 21:23:25 -0800

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class Ticket {
     private String ticketID;
     private Date entryTime;
@@ -5,6 +7,37 @@ public class Ticket {
     private int totalMinutes;
     private int fee;
 
+    public Ticket(String ticketID, Date entryTime) {
+        this.ticketID = ticketID;
+        this.entryTime = entryTime;
+        this.exitTime = null;
+        this.totalMinutes = 0;
+        this.fee = 0;
+    }
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public Date getEntryTime() {
+        return entryTime;
+    }
+
+    public Date getExitTime() {
+        return exitTime;
+    }
+
+    public int getTotalMinutes() {
+        return totalMinutes;
+    }
+
+    public int getFee() {
+        return fee;
+    }
+
     public void markPaid() { }
-    public boolean isValid() { return false; }
+
+    public boolean isValid() {
+        return false;
+    }
 }
